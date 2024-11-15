8020ee717cd46c16a9707081740caff572f108cf
Andrew Veness <7mybqj2j@students.codeinstitute.net>
2024-11-15 15:54:42 +0000

    update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,9 @@
-const test1 = '1QA8YRYtg4rZZmFHs1jMfDWSzaXsrj4jcv'; // test1
-const test2 = '1L2BqGYybxZHMmRJBJqA4Ztyfzv3TbPMw'; // test2
-
-// require('dotenv').config({ path: './wallet.env' });
-
+// require('dotenv').config();
 // const privateKey = process.env.PRIVATE_KEY;
 
-const speak = document.getElementById("box");
+const test1 = '1QA8YRYtg4rZZmFHs1jMfDWSzaXsrj4jcv'; // test1
+const test2 = '1L2BqGYybxZHMmRJBJqA4Ztyfzv3TbPMw'; // test2
+const txBox = document.getElementById("box");
 
 // mainnet
 const mainUrl = "https://api.koinos.io";
@@ -18,7 +16,7 @@ const koinTestId = "1EdLyQ67LW6HVU1dWoceP4firtyz77e37Y";
 (async () => {
   // define signer, provider, and contract
   const provider = new Provider([mainUrl]);
-  const signer = Signer.fromWif();
+  const signer = Signer.fromWif(pk);
   signer.provider = provider;
   const koinContract = new Contract({
     id: koinId,
@@ -62,7 +60,11 @@ const koinTestId = "1EdLyQ67LW6HVU1dWoceP4firtyz77e37Y";
   window.transferKoin = async function transferKoin() {
     try {
       // Specify the recipient address (replace with the actual address)
-      const recipient = test2; // Define recipient address here
+      const recipient = document.getElementById("address").value; // Define recipient address here
+      if (!recipient) {
+        alert("Please enter a valid address.");
+        return;
+      }
 
       // Get the amount to send from the input box
       const amountInput = document.getElementById("amount").value;
@@ -73,6 +75,8 @@ const koinTestId = "1EdLyQ67LW6HVU1dWoceP4firtyz77e37Y";
 
       // Convert the amount to the smallest unit (e.g., multiplying by 10^8 for 8 decimal places)
       const amountToSend = (parseFloat(amountInput) * 1e8).toFixed(0);
+
+      modal.style.display = "none";
 
       // Show loading bar at the start of the transfer
       showLoadingBar();
@@ -95,8 +99,8 @@ const koinTestId = "1EdLyQ67LW6HVU1dWoceP4firtyz77e37Y";
       updateLoadingBar(75);
 
       // Display the block number in the div with ID 'tx'
-      speak.style.background = "#00800045";
-      speak.style.visibility = 'visible';
+      txBox.style.background = "#00800045";
+      txBox.style.visibility = 'visible';
       document.getElementById('tx').innerText = `Transaction mined. Block number: ${blockNumber}`;
       console.log(`Transaction mined. Block number: ${blockNumber}`);
 
@@ -117,8 +121,8 @@ const koinTestId = "1EdLyQ67LW6HVU1dWoceP4firtyz77e37Y";
       // Handle any errors that occur during the transfer
       console.error("Transfer failed:", error);
       // Show the element
-      speak.style.background = "#ff00007a";
-      speak.style.visibility = 'visible';
+      txBox.style.background = "#ff00007a";
+      txBox.style.visibility = 'visible';
       document.getElementById('tx').innerText = `Transfer failed: ${error.message}`;
 
       // Hide the loading bar on error
@@ -127,16 +131,77 @@ const koinTestId = "1EdLyQ67LW6HVU1dWoceP4firtyz77e37Y";
   }
 })();
 
-// Set up event listeners after the DOM is fully loaded
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("Document fully loaded and parsed!");
+// Get the modal
+const modal = document.getElementById("myModal");
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+const showTx = () => {
+
+  const recipient = document.getElementById("address").value;
+  if (!recipient) {
+    alert("Please enter a valid address.");
+    return;
+  }
+
+  const amountInput = document.getElementById("amount").value;
+  if (!amountInput || isNaN(amountInput) || parseFloat(amountInput) <= 0) {
+    alert("Please enter a valid amount.");
+    return;
+  }
+
+  let modelTx = document.getElementById("modal-tx");
+  let modelAmount = document.getElementById("modal-amount");
+  let mdButton = document.getElementById("md-button");
+  modelTx.innerHTML = "";
+  modelAmount.innerHTML = "";
+  mdButton.innerHTML = "";
+
+  if (modal) {
+    let amountDiv = document.createElement("div");
+    amountDiv.id = "amount-div";
+    let txDiv = document.createElement("div");
+    txDiv.id = "tx-div";
+    amountDiv.innerText = `Send ${amountInput} koin`;
+    txDiv.innerText = `To: ${recipient}`;
+
+    let modalButton = document.createElement("button");
+    modalButton.className = "btn--purple";
+    modalButton.id = "send-confirm";
+    modalButton.innerText = "Send";
+
+    modelAmount.appendChild(amountDiv);
+    modelTx.appendChild(txDiv);
+    mdButton.appendChild(modalButton);
+  }
+
+  modal.style.display = "block";
 
   // Attach the transferKoin function to the button with ID 'send'
-  const sendButton = document.getElementById('send');
+  const sendButton = document.getElementById('send-confirm');
   if (sendButton) {  // Check if element exists to avoid errors
     sendButton.addEventListener('click', transferKoin);
   } else {
     console.error("Send button not found");
+  }
+}
+
+// Set up event listeners after the DOM is fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Document fully loaded and parsed!");
+
+  document.getElementById("send").addEventListener("click", showTx);
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
   }
 });
 
